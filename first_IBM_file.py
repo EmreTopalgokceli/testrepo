5e3eab08b80fdfff6e4c221c254504a3d663801e
Emre-Topalgokceli <94282435+EmreTopalgokceli@users.noreply.github.com>
2022-07-26 23:49:39 +0300

    First_IBM_file

---
+++ first_IBM_file.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/Logos/organization_logo/organization_logo.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Emre Topalgokceli
+# Data Scientist
+
+# *I am interested in data science because I love working with data and uncovering patters data has.
+
+# ### My code must has an effect. There should be something happened.
+
+# In[1]:
+
+
+4**2
+
+
+# <blockquote>Hello AI Era!</blockquote>
+# 
+# <ol>
+# <li>One</li>
+# <li>Two</li>
+# <li>Foro</li>
+# </ol>
+# 
+# \$\sqrt{k}$
